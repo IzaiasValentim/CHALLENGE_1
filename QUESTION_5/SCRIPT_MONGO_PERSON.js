c0e9e2b6327015be804b4bd841f4805c768e7116
Izaias <izaias451@gmail.com>
2023-08-20 20:39:50 -0300

    Added Question-05

---
+++ QUESTION_5/SCRIPT_MONGO_PERSON.js
@@ -0,0 +1,22 @@
+db.Person.insertOne(
+
+{
+     "name":"Eu Euzinho",
+     "age":23,
+     "phone":"114002-8922",
+     "height":1.8,
+     "email":"zinho.pb@compasso.com.br",
+     "CPF":"123.456.789-10",
+     "birthday":new Date("2023-08-17"),   
+     "address":{
+         
+          "zip":"101010-000",
+          "street":"Rua Alguma",
+          "complement":"Apto 108",
+          "neighborhood":"Centro",
+          "city":"Pau dos Ferros",
+          "state":"Rio Grande do Norte",
+          "country":"Brasil"
+   }
+   
+});
